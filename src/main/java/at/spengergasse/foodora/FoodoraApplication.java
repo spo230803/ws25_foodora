1519f100f784f20ca07c107bdd32274700a41f3f
spo230803 <spo230803@spengergasse.at>
2025-11-13 09:07:22 +0100

    test 2

---
+++ src/main/java/at/spengergasse/foodora/FoodoraApplication.java
@@ -0,0 +1,13 @@
+package at.spengergasse.foodora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodoraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/spengergasse/foodora/FoodoraApplicationTests.java
@@ -0,0 +1,13 @@
+package at.spengergasse.foodora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
